008001894c601f8829c663003cf86abb51247926
unknown <hojjatrasti1380@gmail.com>
2025-05-29 20:51:54 +0330

    Englishsize admin-blog

--- resources/views/admin/articles/add.blade.php
+++ resources/views/admin/articles/add.blade.php
@@ -5,40 +5,41 @@
 
 
     <!-- upload form -->
-    <div id="webinar-up" class="col-12 col-lg-9 float-start pe-5 ps-5 " dir="ltr">
+    <div id="webinar-up" class="col-12 col-lg-9 float-end pe-5 ps-5 " >
         {{--page title --}}
-        <nav aria-label="breadcrumb" class="d-flex flex-row-reverse mt-3 fs-3 fw-bold" dir="ltr">
+        <nav aria-label="breadcrumb" class="d-flex mt-3 fs-3 fw-bold" >
             <ol class="breadcrumb">
-            <li class="breadcrumb-item" aria-current="page">افزودن مقاله</li>
+            <li class="breadcrumb-item" aria-current="page">Upload Blog</li>
             </ol>
         </nav>
         @include('errors.message')
         <form action="{{ route('admin.post.store') }}" id="article" method="post" class="d-flex justify-content-center flex-wrap mt-5" enctype="multipart/form-data">
             @csrf
             <div class="input-group mb-3 w-100" >
+                <!-- title input -->
+                <input type="text" class="form-control text-center" placeholder="Title" name="title" required autofocus>
+
+                <!-- middle pic -->
+                <span class="input-group-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 64 64"><g transform="rotate(180 32 32)"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2zm0 57.5C16.836 59.5 4.5 47.164 4.5 32S16.836 4.5 32 4.5c15.163 0 27.5 12.336 27.5 27.5S47.163 59.5 32 59.5z"/><circle cx="20.5" cy="26.592" r="5" fill="currentColor"/><circle cx="43.5" cy="26.592" r="5" fill="currentColor"/><path fill="currentColor" d="M44.584 40.279c-8.11 5.656-17.106 5.623-25.168 0c-.97-.677-1.845.495-1.187 1.578c2.458 4.047 7.417 7.65 13.771 7.65s11.313-3.604 13.771-7.65c.658-1.083-.217-2.254-1.187-1.578"/></g></svg>
+                </span>
+                
                 <!-- category input -->
                 <div class="btn-group form-control" >
-                        <select class="form-select text-center cat-multi-select " multiple name="cat[]" id="inputGroupSelect03" aria-label="Example select with button addon" dir="rtl">
+                        <select class="form-select text-center cat-multi-select " multiple name="cat[]" id="inputGroupSelect03" aria-label="Example select with button addon">
                     @foreach($categories as $category)
                         <option value="{{$category->id}}">{{$category->title}}</option>
                         @endforeach
                       </select>
                 </div>
 
-                <!-- middle pic -->
-                <span class="input-group-text">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 64 64"><g transform="rotate(180 32 32)"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2zm0 57.5C16.836 59.5 4.5 47.164 4.5 32S16.836 4.5 32 4.5c15.163 0 27.5 12.336 27.5 27.5S47.163 59.5 32 59.5z"/><circle cx="20.5" cy="26.592" r="5" fill="currentColor"/><circle cx="43.5" cy="26.592" r="5" fill="currentColor"/><path fill="currentColor" d="M44.584 40.279c-8.11 5.656-17.106 5.623-25.168 0c-.97-.677-1.845.495-1.187 1.578c2.458 4.047 7.417 7.65 13.771 7.65s11.313-3.604 13.771-7.65c.658-1.083-.217-2.254-1.187-1.578"/></g></svg>
-                </span>
-                <!-- title input -->
-                <input type="text" class="form-control text-center" placeholder="عنوان" name="title" required autofocus>
-
 
             </div>
             <div class="col-xl-8 col-12 mb-3">
                 {{-- author input --}}
-                <input type="text" class="form-control text-center mb-3 col-12" placeholder="نویسنده" name="author" required>
-                <input type="text" class="form-control text-center mb-3 col-12" placeholder="متا تایتل" name="meta_title" required>
-                <input type="text" class="form-control text-center mb-3 col-12" placeholder="متا دیسکریپشن" name="meta_description" required>
+                <input type="text" class="form-control text-center mb-3 col-12" placeholder="Author" name="author" required>
+                <input type="text" class="form-control text-center mb-3 col-12" placeholder="Meta Title" name="meta_title" required>
+                <input type="text" class="form-control text-center mb-3 col-12" placeholder="Meta Description" name="meta_description" required>
 
             </div>
             <!-- Schema input -->
@@ -48,12 +49,12 @@
 
             <!-- discription input -->
             <div class="col-8">
-                <textarea class="form-control" required rows="2" cols="8" dir="rtl" placeholder="توضیحات" name="abstract"></textarea>
+                <textarea class="form-control" required rows="2" cols="8" placeholder="Description" name="abstract"></textarea>
             </div>
             <!-- file upload area -->
             <div class="col-10 mt-3 text-center">
-                <label for="thumbnail_url">افزودن تامبنیل مقاله</label>
-                <input class="form-control mt-3" type="file" id="formFileMultiple" name="thumbnail_url" multiple title="افزودن تامبنیل">
+                <label for="thumbnail_url">Blog Thumbnail</label>
+                <input class="form-control mt-3" type="file" id="formFileMultiple" name="thumbnail_url" multiple title="Add Thumbnail">
 
                 {{-- quill text editor --}}
 
@@ -111,7 +112,7 @@
                 <input type="hidden" name="quillBody">
                 <!-- submit btn -->
                 <div class="col-auto mt-3">
-                    <button type="submit" class="btn btn-primary mb-3 ps-5 pe-5 pt-2 pb-2 ">ثبت</button>
+                    <button type="submit" class="btn btn-primary mb-3 ps-5 pe-5 pt-2 pb-2 ">Submit</button>
                 </div>
 
             </div>
